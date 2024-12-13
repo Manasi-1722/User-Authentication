a26272067851e0de46cc218e6c4cc24958e8a7cc
Mansi Yelgulwar <mansiyelgulwar1722@gmail.com>
2024-12-13 13:31:07 +0530

    final

--- src/app/api/users/login/route.js
+++ src/app/api/users/login/route.js
@@ -2,33 +2,33 @@ import User from "@/models/user";
 import bcryptjs from 'bcryptjs';
 import Connection from "@/database/config";
 import jwt from "jsonwebtoken";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 
 // Database connection is established
 Connection(); 
 
-export const POST = async (NextFetchEvent) => {
+export const POST = async (req) => {
     try {
-        const body = await NextFetchEvent.json();
+        const body = await req.json();
 
         const { username, password } = body;
 
         // If any field is not given - Error
         if(!username || !password) {
-            return new Response("Username and Password is required", { status: 401 });
+            return new Response("Username and Password is required", { status: 400 });
         }
 
         // when username is already exist - Error
         const user = await User.findOne({ username });
         if(!user) {
-            return new Response("Username does not exist", { status: 400 });
+            return new Response("Username does not exist", { status: 404 });
         }
 
         // check if given password is matched with original password saved in Database
         const validPassword = await bcryptjs.compare(password, user.password);
         if(!validPassword) {
-            return new Response("Incorrect Password", { status: 400 });
+            return new Response("Incorrect Password", { status: 401 });
         }
 
         const tokenData = {
@@ -40,11 +40,11 @@ export const POST = async (NextFetchEvent) => {
 
         const response = NextResponse.json({ message: 'Login successful'});
 
-        response.cookies.set("token", token, { httpOnly: true });
+        response.cookies.set("token", token, { httpOnly: true, secure: true, sameSite: "strict" });
         return response;
 
     } catch (error) {
-        console.log("ERROR", error.message);
-        return new Response("Something went wrong ", { status: 500 })
+        console.error("Error occurred during login:", error.stack || error.message);
+        return new Response("Something went wrong: " + error.message, { status: 500 });
     }
 }
